e979d0b662d64beacd531394f590293de799e3a4
Adwait Nagarkar <109037318+Adwait47s@users.noreply.github.com>
2023-08-22 13:09:17 +0530

    Update program.c

--- OS_LABS/Assignment 3/program.c
+++ OS_LABS/Assignment 3/program.c
@@ -3,11 +3,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <cstring>
+
 const int N = 1001;
-int process[N];
-int time[N];
-int vis[N];
+int process[1001];
+int time[1001];
+int vis[1001];
+int sysprocess[1001];
 int size = 0;
+int arrival_time[N], burst_time[N], priority[N], qt[N];
 void prnt()
     {
         for (int i = 0; i < size - 1; i++)
@@ -16,6 +20,7 @@ void prnt()
             printf("%d  =  ", time[i + 1]);
             printf("%d\n", process[i]);
         }
+        printf("\n\n");
         int upto = time[size-1];
         int now=0;
         for(int i=0;i<=upto;i++){
@@ -93,7 +98,24 @@ void prnt()
             }
             
         }
-        printf("\n");
+        printf("\n\n");
+        now=1;
+        for(int j=0;j<size;j++){
+            for(int i=0;i<size;i++){
+                if(now==process[i]){
+                    printf("Process");
+                    printf(" %d" ,now);
+                    printf("  %d  " , arrival_time[now-1]);
+                    printf("  %d  " , burst_time[now-1]);
+                    int tat = time[now-1] - time[now-1-1] - arrival_time[now-1];
+                    printf("  %d  " , tat);
+                    printf("  %d  \n" , tat -burst_time[now-1]);
+                    now++;
+                    break;
+                }
+            }
+        }
+        
     }
 
 int main()
@@ -101,7 +123,7 @@ int main()
     int n;
     printf("Enter the number of processes in the array: ");
     scanf("%d", &n);
-    int arrival_time[N], burst_time[N], priority[N], qt[N];
+    
 
     printf("Enter the arrival time of each process : ");
 
@@ -135,10 +157,12 @@ int main()
     
 
     while (1)
-    {
-        printf("1.First Come First Serve");
+    {   printf("----------MENU------------\n\n");
+        printf("1.First Come First Serve\n");
+        printf("2.Shortest Job First\n");
+        printf("\n");
         printf("Please make a choice\n");
-
+        printf("\n\n");
         int choice;
         scanf("%d", &choice);
         int currT = 1, currP = 0;
@@ -187,12 +211,74 @@ int main()
             prnt();
         }
         else if(choice==2){
+            memset(process,0,sizeof(process));
+            memset(time,0,sizeof(time));
+            memset(vis,0,sizeof(vis));
+            size=0;
+            currT = 1, currP = 0;
+            int cnt = 0;
+            time[0] = 0;
+
+            while (cnt <= n)
+            {
+                int timetillnow = time[currT-1];
+                int pr = -1;
+                int bt = 1001; // burst time of that process
+                int mininum_next=1001;
+                int prnext =-1;
+                for (int i = 0; i < n; i++)
+                {
+                    if (timetillnow >= arrival_time[i] && vis[i] == 0)
+                    {
+                        if(burst_time[i]<bt){
+                            pr = i;
+                            bt = burst_time[i];
+                        }
+                    }
+                    else if(timetillnow < arrival_time[i] && vis[i] == 0){
+                        if(mininum_next>arrival_time[i]){
+                            prnext = i;
+                            mininum_next = arrival_time[i];
+                        }
+                    }
+                }
+
+                vis[pr] = 1;
+
+                if (pr!=-1)
+                {
+                    time[currT] = time[currT - 1] + burst_time[pr];
+                    process[currP] = pr + 1;
+                    currT++;
+                    currP++;
+                    size++;
+                }
+                else
+                {
+                    time[currT] = arrival_time[pr];
+                    process[currP] = -1;
+                    currT++;
+                    currP++;
+                    time[currT] = time[currT - 1] + burst_time[pr];
+                    process[currP] = pr + 1;
+                    currT++;
+                    currP++;
+                    size += 2;
+                }
+
+                cnt++;
+            }
+            size--;
+            prnt();
+        }
+        else if(choice==3){
 
         }
         else
         {
             break;
         }
+        printf("\n\n");
     }
 }
 
